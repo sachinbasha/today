f8c40f2d6550daadf8cbcd65d44592fa84ca1feb
uchesachin96@gmail.com <uchesachin96@gmail.com>
2023-12-25 23:15:48 +0530

    ok

---
+++ src/test/java/seleniumgrid/SeleniumGrid.java
@@ -0,0 +1,30 @@
+package seleniumgrid;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SeleniumGrid {
+
+	@Test
+	public void testTest() throws URISyntaxException, MalformedURLException, InterruptedException
+	{
+		URL url=new URL("http://192.168.226.1:5555/wd/hub");
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setBrowserName("chrome");
+		cap.setPlatform(Platform.WINDOWS);
+		Thread.sleep(2000);
+		Reporter.log("chrome");
+		RemoteWebDriver  driver=new RemoteWebDriver(url,cap);
+		driver.get("https://googlechromelabs.github.io/chrome-for-testing/#stable");
+		
+	}
+}
---
+++ src/test/java/seleniumgrid/ZenkinTest.java
@@ -0,0 +1,14 @@
+package seleniumgrid;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ZenkinTest 
+{
+	@Test
+	public void test()
+	{
+		Reporter.log("sachin",true);
+	}
+
+}
